4ba95df15c3b856b263141aed9907c7f8a414d90
asilbekali <asilbekabdugafforov7@gmail.com>
2025-12-28 16:11:48 +0500

    new branch joined to project

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ const page = () => {
       <div>
         <p>Home Page</p>
         <Button variant="contained">MUI Button</Button>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error consequatur dolores rerum corrupti tempora! Iure aliquam dolorum tenetur corporis esse non beatae! Esse sequi fugit ipsum dolores quaerat quas, dolorem quisquam aspernatur totam soluta aliquam tenetur praesentium, possimus, ex et est sint nisi accusamus distinctio nostrum corrupti explicabo? Fugit dolorum itaque eos suscipit reiciendis, omnis consequuntur perferendis. Voluptate amet nihil recusandae laborum libero eligendi sunt. Suscipit eligendi quia natus nihil totam sunt, ducimus, possimus tenetur facere dolorem dolorum consequatur aperiam earum repudiandae eaque placeat eos magnam quis distinctio? Atque ea voluptatem voluptates sunt! Excepturi labore ea mollitia amet beatae?</p>
       </div>
     </>
   );
